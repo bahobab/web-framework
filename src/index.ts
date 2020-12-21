9f3a57f79e591c86e7d96c0fb57ec6a3623ac1c7
Konan Houphoue <bahobab@hotmail.com>
2020-12-20 21:45:29 -0600

    resolved 'this' context issue

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import axios from 'axios';
-
 import { User } from './models/User';
 
 const mamadou = new User({ name: 'Mamadou', age: 30 });
+
+console.log(mamadou.get('name'));
+
+mamadou.on('change', () => console.log('Hey! Change?'));
+mamadou.trigger('change');
--- src/models/Eventing.ts
+++ src/models/Eventing.ts
@@ -3,13 +3,13 @@ type Callback = () => void;
 export class Eventing {
   events: {[key:string]:Callback[]} = {};
 
-  on(eventName: string, callback: Callback): void {
+  on = (eventName: string, callback: Callback): void => {
     const eventHandlers = this.events[eventName] || [];
     eventHandlers.push(callback);
     this.events[eventName] = eventHandlers;
   }
 
-  trigger(eventName: string): void {
+  trigger = (eventName: string): void => {
     if (!this.events[eventName] || this.events[eventName].length === 0) {
       return;
     }
