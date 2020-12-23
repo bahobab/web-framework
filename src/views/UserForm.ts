232fa090e90e005b9c60f183fa3e0766fc1b7f10
Konan Houphoue <bahobab@hotmail.com>
2020-12-22 19:54:21 -0600

    implemented eventing

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -2,11 +2,39 @@
 export class UserForm {
   constructor(public parent: Element) {};
 
+  eventsMap(): {[key: string]: () => void} {
+    return {
+      'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onH1Hover
+    };
+  }
+
+  bindEvents(fragment: DocumentFragment):void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element: Element) => {
+        element.addEventListener(eventName, eventsMap[eventKey])
+      })
+    }
+  }
+
+  onH1Hover(): void {
+    console.log('H1 was hovered over');
+  }
+
+  onButtonClick(): void {
+    console.log('button clicked!')
+  }
+
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
         <input />
+        <button>Click Me</button>
       </div>
     `
   };
@@ -14,6 +42,8 @@ export class UserForm {
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
